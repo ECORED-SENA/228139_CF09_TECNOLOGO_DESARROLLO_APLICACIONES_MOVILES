fd19fd853a2c89b16e9e7a555ea757a147097010
emilydev <98632409+emilydev@users.noreply.github.com>
2023-09-11 15:38:53 -0500

    Se carga video.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,8 +13,8 @@
 
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video.
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/iXR0zkUVsoQ?si=0ce1JfLSx_tSJdp2" title="Aplicaciones móviles híbridas" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video.Aplicaciones móviles híbridas
 
 </template>
 
